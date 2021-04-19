2de9544ad14c34e2e8c0d53416730f2aa9e48d9f
Cyssoo <cyrilchalamon@gmail.com>
2021-04-19 16:37:08 +0200

    Removed useless constant, removed empty lines

--- everwoofreeshippingnotifications.php
+++ everwoofreeshippingnotifications.php
@@ -3,7 +3,7 @@
 Plugin URI: https://www.team-ever.com
 Plugin Name: Everwoo Free Shipping Notifications
 Description: Use this plugin for Showing a ‘Add --- more for free shipping’ 
-Version: 1.1.1
+Version: 1.1.2
 Author: Ever Team
 License: Tous droits réservés / Le droit d'auteur s'applique (All rights reserved / French copyright law applies)
 Text Domain: everwoofreeshippingnotifications
@@ -24,28 +24,19 @@ class everwoofreeshippingnotifications
         $plugin               = new stdClass;
         $plugin->name         = 'everwoofreeshippingnotifications'; // Plugin Folder
         $plugin->displayName  = 'Everwoo Free Shipping Notifications'; // Plugin Name
-        $plugin->version      = '1.1.1';
+        $plugin->version      = '1.1.2';
         $plugin->folder       = plugin_dir_path(__FILE__);
         $plugin->url          = plugin_dir_url(__FILE__);
     }
 }
-
-
-
 /**
  * Loads plugin textdomain
  */
-function everwoofreeshippingnotifications_plugins_loaded()
-{
+function everwoofreeshippingnotifications_plugins_loaded() {
     load_plugin_textdomain('everwoofreeshippingnotifications', false, dirname(plugin_basename(__FILE__)) . '/languages/');
-    if (!defined('SEP')) {
-        define('SEP', ';');
-    }
-
 }
 add_action('plugins_loaded', 'everwoofreeshippingnotifications_plugins_loaded', 0);
-function everwoofreeshippingnotifications_add_admin_menu()
-{
+function everwoofreeshippingnotifications_add_admin_menu() {
     add_options_page('Everwoo Free Shipping Notifications', 'Everwoo Free Shipping Notifications', 'manage_options', 'ever_wp_everwoofreeshippingnotifications', 'everwoofreeshippingnotifications_options_page');
 }
 add_action('admin_menu', 'everwoofreeshippingnotifications_add_admin_menu');
@@ -59,8 +50,7 @@ add_action('admin_menu', 'everwoofreeshippingnotifications_add_admin_menu');
  * @param  array  $links List of existing plugin action links.
  * @return array         List of modified plugin action links.
  */
-function everwoofreeshippingnotifications_action_links($links)
-{
+function everwoofreeshippingnotifications_action_links($links) {
     $icon = plugin_dir_url(__FILE__) . 'assets/img/logo.ico';
     $logo = '<img src="' . $icon . '" class="img img-fluid icon-team-ever" alt="WooCommerce plugin by Team Ever" title="WooCommerce plugin by Team Ever" style="width:20px;height:20px;">';
     $links = array_merge(array(
@@ -70,25 +60,19 @@ function everwoofreeshippingnotifications_action_links($links)
 }
 // Action links
 add_action('plugin_action_links_' . plugin_basename(__FILE__), 'everwoofreeshippingnotifications_action_links');
-function everwoofreeshippingnotifications_settings_init()
-{
+function everwoofreeshippingnotifications_settings_init() {
     register_setting('pluginPage', 'everwoofreeshippingnotifications_settings');
 
-    if (class_exists('WooCommerce')) {
-        add_settings_section(
-            'everwoofreeshippingnotifications_pluginPage_section',
-            __('WordPress & WooCommerce settings', 'everwoofreeshippingnotifications'),
-            'everwoofreeshippingnotifications_settings_section_callback',
-            'pluginPage'
-        );
-    } else {
-        add_settings_section(
-            'everwoofreeshippingnotifications_pluginPage_section',
-            __('WordPress settings', 'everwoofreeshippingnotifications'),
-            'everwoofreeshippingnotifications_settings_section_callback',
-            'pluginPage'
-        );
+    if (!class_exists('WooCommerce')) {
+        // WooCommerce not installed
     }
+
+    add_settings_section(
+        'everwoofreeshippingnotifications_pluginPage_section',
+        __('WordPress & WooCommerce settings', 'everwoofreeshippingnotifications'),
+        'everwoofreeshippingnotifications_settings_section_callback',
+        'pluginPage'
+    );
 
     add_settings_field(
         'everwoofreeshippingnotifications_message_before_amount',
@@ -109,30 +93,26 @@ function everwoofreeshippingnotifications_settings_init()
 
 add_action('admin_init', 'everwoofreeshippingnotifications_settings_init');
 
-
 /**
  * Render message before amount 
  */
-function everwoofreeshippingnotifications_message_before_amount_render()
-{
+function everwoofreeshippingnotifications_message_before_amount_render() {
     $options = get_option('everwoofreeshippingnotifications_settings');
-    if (
-        isset($options['everwoofreeshippingnotifications_message_before_amount'])
+    if (isset($options['everwoofreeshippingnotifications_message_before_amount'])
         && !empty($options['everwoofreeshippingnotifications_message_before_amount'])
     ) {
         $value = $options['everwoofreeshippingnotifications_message_before_amount'];
     } else {
         $value = '';
     }
-?>
+    ?>
     <input type="text" name="everwoofreeshippingnotifications_settings[everwoofreeshippingnotifications_message_before_amount]" value="<?php echo $value; ?>">
-<?php
+    <?php
 }
 /**
  * Render message after amount shipping cart
  */
-function everwoofreeshippingnotifications_message_cart_render()
-{
+function everwoofreeshippingnotifications_message_cart_render() {
     $options = get_option('everwoofreeshippingnotifications_settings');
     if (
         isset($options['everwoofreeshippingnotifications_message_cart'])
@@ -146,21 +126,17 @@ function everwoofreeshippingnotifications_message_cart_render()
     <input type="text" name="everwoofreeshippingnotifications_settings[everwoofreeshippingnotifications_message_cart]" value="<?php echo $value; ?>">
 <?php
 }
-
-function everwoofreeshippingnotifications_settings_section_callback()
-{
+function everwoofreeshippingnotifications_settings_section_callback() {
     if (!class_exists('WooCommerce')) {
         echo __('WooCommerce settings wont work until you install this plugin', 'everwoofreeshippingnotifications');
     } else {
         echo __('Please set form settings to allow or dissallow Everwoo Free Shipping Notifications on your WordPress site', 'everwoofreeshippingnotifications');
     }
 }
-function everwoofreeshippingnotifications_options_page()
-{
+function everwoofreeshippingnotifications_options_page() {
     // Useful : use var_dump($options) for seeing array of settings values
     $options = get_option('everwoofreeshippingnotifications_settings');
-    //var_dump($options);
-?>
+    ?>
     <div class="jumbotron">
         <a href="https://www.team-ever.com/contact" target="_blank"><img src="https://www.team-ever.com/wp-content/uploads/2016/08/Logo-full.png" style="float:left;"></a>
         <h1><?php _e('everwoofreeshippingnotifications settings', 'everwoofreeshippingnotifications'); ?></h1>
@@ -191,16 +167,10 @@ function everwoofreeshippingnotifications_options_page()
             eversetAmountForFreeShippin();
         }
     ?>
-      
-<?php
+    <?php
     }
-
-    
 }
-
 function everSetAmountForFreeShippin() {
-   
-
 	if ( ! is_cart() && ! is_checkout() ) { 
 		return;
 	}
@@ -217,24 +187,11 @@ function everSetAmountForFreeShippin() {
 	}
 	foreach ( $zone->get_shipping_methods( true ) as $k => $method ) {
 		$min_amount = $method->get_option( 'min_amount' );
-
-
 		if ( $method->id == 'free_shipping' && ! empty( $min_amount ) && $cart_total < $min_amount ) {
 			$remaining = $min_amount - $cart_total;
             $options = get_option( 'everwoofreeshippingnotifications_settings' );
 			wc_add_notice( $message= ($options['everwoofreeshippingnotifications_message_before_amount'].'&nbsp; '.wc_price( $remaining ).' &nbsp;'.$options['everwoofreeshippingnotifications_message_cart']), 'success' ) ;
-         
-         /* $message = '<p style="text-align: center;font-size: 2em;">'.($options['everwoofreeshippingnotifications_message_before_amount'].'&nbsp; '.wc_price( $remaining ).' &nbsp;'.$options['everwoofreeshippingnotifications_message_cart']) .'</p>';
-            echo $message; */
-         
-
-           
 		}
 	}
-
-    
-  
-
 }
 add_action( 'woocommerce_before_cart', 'everSetAmountForFreeShippin', 10 );
-
